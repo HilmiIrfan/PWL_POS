5e8acdc322c28d226265728e8ac1ea93c2408b2f
HilmiIrfan <hilminaafiudin@gmail.com>
2024-03-08 10:10:36 +0700

    Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index(){
         $data=[
-            'nama'=>'Pelanggan Pertama',
+            'level_id'=>2,
+            'username'=>'manager_tiga',
+            'nama'=>'Manager 3',
+            'password'=>Hash::make('12345'),
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
         $user=UserModel::all();
         return view('user', ['data'=>$user]);
     }
